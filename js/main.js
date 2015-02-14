05f47504308033749abed085137c8d5ce620f227
bbhamilton <bartekbilicki@gmail.com>
2015-02-14 20:23:37 +0100

    grid system in projects

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+$(function(){
+    console.log('init');
+  $(window).on('resize', function(e){
 
+    $('.tile').each(function(){
+      var width = $(this).width();
+
+      $(this).find('div').text('width: ' + width);
+
+    });
+  });
+});
