cbea014fa0344d4b304e91828f44febee6833bb9
bbhamilton <bartekbilicki@gmail.com>
2015-02-14 22:08:51 +0100

    projects tiles #1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
 $(function(){
-    console.log('init');
-  $(window).on('resize', function(e){
 
-    $('.tile').each(function(){
-      var width = $(this).width();
-
-      $(this).find('div').text('width: ' + width);
-
-    });
-  });
 });
